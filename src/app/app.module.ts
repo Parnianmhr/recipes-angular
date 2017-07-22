05e9e739a9ce774da41f79dfc559b1e6156bbcd9
Parnian <parnian@Parnians-MacBook-Pro.local>
2017-07-22 17:05:19 +0200

    components recipes and header added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
 import { FormsModule } from '@angular/forms'
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
 import { WarningAlertComponent } from './warning/warning.component'
 import { SuccessAlertComponent } from './success/success.component'
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     WarningAlertComponent,
-    SuccessAlertComponent
+    SuccessAlertComponent,
+    HeaderComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule,
