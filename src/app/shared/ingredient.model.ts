504eb9e47c0c4a52a24f22d3f5f5365a78118006
Parnian <parnian@Parnians-MacBook-Pro.local>
2017-07-23 11:54:35 +0200

    creating ingredient model

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,15 @@
+export class Ingredient {
+  constructor(public name: string, public amount: number) {}
+}
+
+//code above dose exactly what the code below dose:
+
+// export class Ingredient {
+//   public name: string;
+//   public amount: number;
+//
+//   constructor(name: string, amount: number) {
+//     this.name = name;
+//     this.amount = amount;
+//   }
+// }
